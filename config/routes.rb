2e7868b08412349e3b828e11d0b9755bda7e633d
Ian Thorp <IanMThorp@gmail.com>
2016-05-08 13:33:52 -0700

    Add user registration/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+
   get 'welcome/index'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/users/:id' => 'users#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
